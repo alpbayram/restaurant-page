a2b91a9525eca1a93514387014df3ec9566467e3
AlpBayram <57071283+alpbayram@users.noreply.github.com>
2025-04-25 22:29:54 +0300

    menu tasarımı tamamladım.Responsive hale getirdim.

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -3,19 +3,17 @@ import Image from "../images/cookie-hero-low.avif";
 export default function renderHome() {
 	const content = document.querySelector("#content");
 	const gridContainer = document.querySelector(".grid-container");
-    const url = `url("${Image}")`;
-	gridContainer.style.backgroundImage = url
+	const url = `url("${Image}")`;
+	gridContainer.style.backgroundImage = url;
 
 	// linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.0)),
-	
+
 	const heroAlignContainer = document.createElement("div");
 
 	const heroContainer = document.createElement("div");
 	const heroText1 = document.createElement("h6");
 	const heroText2 = document.createElement("h1");
 	const heroText3 = document.createElement("h6");
-    
-   
 
 	gridContainer.dataset.status = "home";
 
@@ -24,11 +22,12 @@ export default function renderHome() {
 	heroText1.classList.add("hero-text-1");
 	heroText2.classList.add("hero-text-2");
 	heroText3.classList.add("hero-text-3");
-    heroText1.textContent="WELCOME TO"
- heroText2.textContent="SEZİŞ COOKIEŞ"
-  heroText3.textContent="Cookie. Coffe. Tea. Sweets."
+	heroText1.textContent = "WELCOME TO";
+	heroText2.textContent = "SEZİŞ COOKIEŞ";
+	heroText3.textContent = "Cookie. Coffe. Tea. Sweets.";
 	content.appendChild(heroAlignContainer);
 	heroAlignContainer.appendChild(heroContainer);
 	heroContainer.appendChild(heroText1);
-	heroContainer.appendChild(heroText2);heroContainer.appendChild(heroText3);
+	heroContainer.appendChild(heroText2);
+	heroContainer.appendChild(heroText3);
 }
